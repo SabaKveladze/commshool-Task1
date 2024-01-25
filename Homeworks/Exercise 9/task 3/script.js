e3afaa5895a55c0a9d9dddec439615bab9029381
Saba Kveladze <sabaqveladze16@gmail.com>
2024-01-25 17:01:55 +0400

    Finished

--- Homeworks/Exercise 9/task 3/script.js
+++ Homeworks/Exercise 9/task 3/script.js
@@ -1,1 +1,19 @@
-alert("Who's There?")
+
+
+function authorization() {
+    let begin = prompt("Who's There")
+    if (begin == null || begin == "") {
+        alert("User cancelled the prompt.")
+      } else if( begin != "Admin") {
+       alert("I don't Know You")
+      } else if(begin == "Admin") {
+        let pass = prompt("Enter Your Password: ")
+        if (pass == "TheMaster") {
+            alert("Welcome")
+        }else if (pass == null || pass == "") {
+            alert("User cancelled the prompt.")
+          } else if(pass != "TheMaster") {
+            alert("Wrong Password")
+      }
+    }
+}
