e2d5a2cb0e1645fcc3fc83ad83fee5046029d280
Saba Kveladze <sabaqveladze16@gmail.com>
2024-01-27 15:45:37 +0400

    Update

---
+++ Homeworks/Exercise 10/task1.js
@@ -0,0 +1,9 @@
+// let browser;
+// if (browser === 'Edge') {
+//     alert("You've got the Edge! ");
+// }else if (browser === 'Chrome' || browser === 'FireFox'
+//             || browser === 'Safari' || browser === 'Opera') {
+//     alert('okay we support these browsers too')
+// }else {
+//     alert('we hope that this page looks ok!')
+// }
