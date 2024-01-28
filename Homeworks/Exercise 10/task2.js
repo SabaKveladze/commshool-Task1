0fd62a6afc8f6600fad24cbf772a8a02462d4cea
Saba Kveladze <sabaqveladze16@gmail.com>
2024-01-28 20:34:32 +0400

    Update

--- Homeworks/Exercise 10/task2.js
+++ Homeworks/Exercise 10/task2.js
@@ -1,17 +1,17 @@
-let num1 = +prompt("NUMBER 1 = ?")
-let num2 = +prompt("NUMBER 2 = ?")
-let operation = prompt("which operation do you want? hint: use these options --> +,-,*,/")
-let answer;
-function calculator(num1,num2,operation) {
-    if(operation == "+"){
-        answer = alert("Answer = " + num1 + num2)
-    }else if(operation == "-"){
-        answer = alert("Answer = " + num1 - num2)
-    }else if(operation == "*"){
-        answer = alert("Answer = " + num1 * num2) 
-    }else if(operation == "/"){
-        answer = alert("Answer = " + num1 / num2) 
-    } 
-    return answer;
-}
-console.log (calculator(num1,num2,operation))
+// let num1 = +prompt("NUMBER 1 = ?")
+// let num2 = +prompt("NUMBER 2 = ?")
+// let operation = prompt("which operation do you want? hint: use these options --> +,-,*,/")
+// let answer;
+// function calculator(num1,num2,operation) {
+//     if(operation == "+"){
+//         answer = alert("Answer = " + num1 + num2)
+//     }else if(operation == "-"){
+//         answer = alert("Answer = " + num1 - num2)
+//     }else if(operation == "*"){
+//         answer = alert("Answer = " + num1 * num2) 
+//     }else if(operation == "/"){
+//         answer = alert("Answer = " + num1 / num2) 
+//     } 
+//     return answer;
+// }
+// console.log (calculator(num1,num2,operation))
---
+++ Homeworks/Exercise 10/task3.js
@@ -0,0 +1,15 @@
+
+
+function inputNumberType() {
+    let num
+    do {
+         num = +prompt("Number = ")
+         console.log(num, typeof num)
+         if(isNaN(num)){
+             alert("Incorrect Type, Write Number")
+        } 
+    }while(
+         isNaN(num)
+    );
+}
+console.log(inputNumberType())
